159aff00f9f57e10301cc342ba2231227d63e162
bluwy <bjornlu.dev@gmail.com>
2026-01-12 18:31:11 +0800

    Release v0.2.4

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // @homepageURL  https://github.com/bluwy/refined-github-comments
 // @supportURL   https://github.com/bluwy/refined-github-comments
 // @namespace    https://greasyfork.org/en/scripts/465056-refined-github-comments
-// @version      0.2.3
+// @version      0.2.4
 // @description  Remove clutter in the comments view
 // @author       Bjorn Lu
 // @match        https://github.com/**
